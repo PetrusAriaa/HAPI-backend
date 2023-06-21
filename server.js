c712b005fb00eff63db4410c54fbdfac92131e4a
PetrusAriaa <petrusariacr25@gmail.com>
2023-06-22 02:06:02 +0700

    page routes dan API endpoints

--- server.js
+++ server.js
@@ -1,11 +1,65 @@
 const Hapi = require('@hapi/hapi');
 
+const HOSTNAME = 'localhost';
+const PORT = 3300;
+
+var userData = [
+	{ name: 'John Doe', age: 25, state: 'Kansans' },
+	{ name: 'Christine', age: 23, state: 'New State' },
+	{ name: 'Dave Richard', age: 28, state: 'Texas' },
+];
+
+var bookAuthors = [
+	{ auth_id: 113, name: 'Pidi Baiq' },
+	{ auth_id: 206, name: 'Tere Liye' },
+	{ auth_id: 313, name: 'Hajime Isayama' },
+];
+
 // inisiasi listened port
 const init = async () => {
 	const server = Hapi.server({
-		port: 3300,
-		host: 'localhost',
+		port: PORT,
+		host: HOSTNAME,
 	});
+
+	// pages route
+	server.route([
+		{
+			path: '/',
+			method: 'GET',
+
+			handler: (req, h) => {
+				return '<h1> Welcome </h1>';
+			},
+		},
+		{
+			path: '/dashboard',
+			method: 'GET',
+			handler: (req, h) => {
+				return '<h1> Dashboard </h1>';
+			},
+		},
+	]);
+
+	// api endpoints
+	server.route([
+		{
+			path: '/api/userData',
+			method: 'GET',
+
+			handler: (req, h) => {
+				return userData;
+			},
+		},
+		{
+			path: '/api/authors',
+			method: 'GET',
+
+			handler: (req, h) => {
+				return bookAuthors;
+			},
+		},
+	]);
 
 	await server.start();
 	console.log(`Server is up on ${server.info.uri}`);
